b40d2e8ec04946f03599e72cc222e7f3b64cc83f
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-01-25 10:35:42 -0500

    Route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,13 @@ import { AdminComponent } from './common/admin.component';
 import { ContactComponent } from './common/contact.component';
 import { HomeComponent } from './common/home.component';
 import { environment } from '../environments/environment';
+import { LoginRouteGuardService } from './services/login-route-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo:'/home', pathMatch:'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [LoginRouteGuardService] },
   { path: 'contact', component: ComposeMessageComponent, outlet: 'side' },
   { path: 'error', component: ErrorComponent },
   { path: 'products', loadChildren: () => 
